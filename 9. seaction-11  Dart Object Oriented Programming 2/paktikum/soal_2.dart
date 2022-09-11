ccff497f709a4229dde9f4b4da4c6ff1b9ee73e1
amrilrisman <amrilrisman@gmail.com>
2022-09-11 20:18:17 +0700

    soal 2 KPT

--- 9. seaction-11  Dart Object Oriented Programming 2/paktikum/soal_2.dart
+++ 9. seaction-11  Dart Object Oriented Programming 2/paktikum/soal_2.dart
@@ -1,12 +1,14 @@
 void main(List<String> args) {
-  int x = 2;
-  int y = 4;
-  int xx = 12;
-  int yy = 20;
+  int x = 100;
+  int y = 10;
+  double xx = 125;
+  double yy = 250;
 
   KPT hasilKPT = KPT(x: x, y: y);
   final resultKPT = hasilKPT.hasil();
   print(resultKPT);
+
+  print('\n*****************************\n');
 
   FPT hasilFPT = FPT(x: xx, y: yy);
   final resultFPT = hasilFPT.hasil();
@@ -14,16 +16,22 @@ void main(List<String> args) {
 }
 
 class FPT extends Matematika {
-  final int x, y;
+  double x, y;
   FPT({required this.x, required this.y});
+  List listX = [];
+  List listY = [];
 
-  int result = 0;
+  late double result;
   @override
   hasil() {
-    while (result % y != 0) {
-      result -= x;
+    result = x % y;
+    while (result != 0) {
+      x = y;
+      y = result;
+      result = x % y;
     }
-    return 'hasil FPT dari $x dan $y adalah => $result';
+
+    return 'hasil FPT dari $x dan $y adalah => $y';
   }
 }
 
