aa690a9eabcc4e925466d5ca0c557c13ad869839
amrilrisman <amrilrisman@gmail.com>
2022-09-27 00:15:37 +0700

    fixed praktikum seaction 17 tentang Form, input Bottom

---
+++ 15 seaction-17 Form Input Button/praktikum/lib/pages/add_contact.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:praktikum/providers/contact_provider.dart';
+import 'package:praktikum/themes/style_all.dart';
+import 'package:provider/provider.dart';
+
+class AddContact extends StatelessWidget {
+  static String rootNamed = '/add-contacts';
+  AddContact({Key? key}) : super(key: key);
+  final TextEditingController _controllerName = TextEditingController();
+  final TextEditingController _controllerNomor = TextEditingController();
+
+  _createNewContact() {
+    return AppBar(
+      title: const Text('Create New Contact'),
+    );
+  }
+
+  _bodyContent(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Icon(
+            Icons.person,
+            color: Colors.blue,
+            size: 150,
+          ),
+          TextFormField(
+            controller: _controllerName,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(
+                Icons.person,
+                color: Colors.blue,
+              ),
+              hintText: 'Maskan Nama Anda',
+              border: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(8)),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          TextFormField(
+            controller: _controllerNomor,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(
+                Icons.call,
+                color: Colors.blue,
+              ),
+              hintText: 'Maskan Nomor',
+              border: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(8)),
+            ),
+          ),
+          const Spacer(),
+          FlatButton(
+            height: 50,
+            color: Colors.blue,
+            onPressed: () {
+              context.read<ContactsProvider>().addCOntact(context, name: _controllerName.text, nomor: _controllerNomor.text);
+            },
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            child: Text(
+              'Submit',
+              style:
+                  h2.copyWith(fontWeight: FontWeight.w600, color: Colors.white),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _createNewContact(),
+      body: _bodyContent(context),
+    );
+  }
+}
