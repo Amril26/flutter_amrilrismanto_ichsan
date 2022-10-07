9fe22570c2e6203aa2f90605c669f67213a311fd
amrilrisman <amrilrisman@gmail.com>
2022-10-07 17:27:31 +0700

    bukti seaction state management bloc

--- 20 seaction-22 Flutter State Management Bloc/praktikum_stf/android/app/src/main/kotlin/com/example/praktikum_stf/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.praktikum_stf
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- 20 seaction-22 Flutter State Management Bloc/praktikum_stf/lib/models/contacts_model.dart
+++
@@ -1,5 +0,0 @@
-class ContactsModel {
-  final String name, nomor;
-
-  ContactsModel({required this.name, required this.nomor});
-}
